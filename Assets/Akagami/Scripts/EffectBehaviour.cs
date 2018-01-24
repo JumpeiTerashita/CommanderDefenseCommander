961eeff770d94541d3ad6b273c442130caacf16f
sho akagami <vant.akagami@gmail.com>
2018-01-24 15:54:57 +0900

    エフェクト誘爆作成中

---
+++ Assets/Akagami/Scripts/EffectBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectBehaviour : MonoBehaviour {
+
+    private void OnParticleCollision(GameObject other)
+    {
+        //Debug.Log(other.transform.tag);
+        if (other.transform.tag == "Enemy")
+        {
+            Collision col = null;
+            
+        }
+    }
+}
