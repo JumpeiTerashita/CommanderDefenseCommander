c23c6f0fa1c9478ffdeca418c5b04287500b5f47
sho akagami <vant.akagami@gmail.com>
2018-01-25 19:41:37 +0900

    プレイヤー製作開始

---
+++ Assets/Akagami/Scripts/GazePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazePlayer : MonoBehaviour {
+
+    // 表示させるための基準オブジェクト情報を保持
+    // 現在はカメラを入れています。
+    [SerializeField]
+    public GameObject baseObj;
+    // 非アクティブの際に外部からアクセスするための変数
+    private static GameObject arrow;
+    [SerializeField]
+    public const float CURSOR_LENGTH = 2;
+    private void Start()
+    {
+        // 自信を保持
+        arrow = this.gameObject;
+    }
+    private void Update()
+    {
+        SetCursorPos();
+        //SetCursorRotation();
+    }
+    // カーソルの位置調整
+    private void SetCursorPos()
+    {
+        if (baseObj == null)
+        {
+            return;
+        }
+        this.transform.position = baseObj.transform.position;
+        this.transform.eulerAngles = baseObj.transform.eulerAngles;
+        float angleDir = this.transform.eulerAngles.y * Mathf.Deg2Rad;
+        Vector3 dir = new Vector3(
+            Mathf.Sin(angleDir),
+            Mathf.Sin(-this.transform.eulerAngles.x * Mathf.Deg2Rad),
+            Mathf.Cos(angleDir));
+        this.transform.position += dir * CURSOR_LENGTH;
+    }
+
+ 
+}
